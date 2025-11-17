8536564279b1fce5e7359e03237a370435befe8b
leonardbantoro <leonardbantoro08@gmail.com>
2025-11-17 16:00:10 +0700

    Commit No. 1: Building App with SharedPreferences and DataStore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,11 +38,15 @@ android {
 }
 
 dependencies {
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
